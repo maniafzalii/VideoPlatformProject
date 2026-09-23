07d91ef00fa8ee8a64bb5f56da3be0ae883d9c0b
Manii <manieskandari05@gmail.com>
2026-09-23 13:56:41 +0330

    models for accounts and docs added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=11, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
+
